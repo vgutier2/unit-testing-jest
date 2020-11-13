45d2809f8d66325c33c1d5c23ba8130e771617a0
Velina Gutierrez <velinagutierrez@Velinas-MBP.lan>
2020-11-13 16:40:48 -0500

    unit tested using jest

---
+++ js/cloneArray.test.js
@@ -0,0 +1,9 @@
+const { expect } = require('@jest/globals')
+const { not } = require('expect')
+const cloneArray = require('./cloneArray')
+
+test('properly clones array', () => {
+    const array = [1, 2, 3]
+    expect(cloneArray(array)).toEqual(array)
+    expect(cloneArray(array)).not.toBe(array)
+})
